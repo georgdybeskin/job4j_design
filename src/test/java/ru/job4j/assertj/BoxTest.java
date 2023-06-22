702b82f86457229c19e66f8aaf95ef4ce3045631
gdybe <g.dybeskin@gmail.com>
2023-06-22 17:27:14 +0300

    3. Утверждения с примитивными типами [#504883  [#504883 #424765]], добавлено по 2 теста на каждый метод

--- src/test/java/ru/job4j/assertj/BoxTest.java
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -9,17 +9,70 @@ class BoxTest {
     void isThisSphere() {
         Box box = new Box(0, 10);
         String name = box.whatsThis();
-        boolean result = box.isExist();
-        int number = box.getNumberOfVertices();
-        double area = box.getArea();
         assertThat(name).isEqualTo("Sphere")
-                .isNotEmpty();
-        assertThat(result).isTrue();
-        assertThat(number).isEqualTo(0)
-                .isLessThan(8);
-        assertThat(area).isGreaterThan(0)
-                .isLessThan(1258);
+                .isNotEmpty()
+                .isNotBlank()
+                .isNotNull();
 
+    }
+
+    @Test
+    void isThisTetrahedron() {
+        Box box = new Box(4, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Tetrahedron")
+                .isNotEmpty()
+                .isNotBlank()
+                .isNotNull();
+    }
+    @Test
+    void whenNumberOfVerticesIsZero() {
+        Box box = new Box(0, 10);
+        int numberOfVertices = box.getNumberOfVertices();
+        assertThat(numberOfVertices).isEqualTo(0)
+                .isLessThan(8)
+                .isNotNull();
+    }
+    @Test
+    void whenNumberOfVerticesIsFour() {
+        Box box = new Box(4, 10);
+        int numberOfVertices = box.getNumberOfVertices();
+        assertThat(numberOfVertices).isEqualTo(4)
+                .isLessThan(8)
+                .isPositive()
+                .isNotNull();
+    }
+    @Test
+    void whenNumberOfVerticesIsExist() {
+        Box box = new Box(4, 10);
+        boolean isExist = box.isExist();
+        assertThat(isExist).isTrue()
+                .isNotNull();
+    }
+    @Test
+    void whenNumberOfVerticesIsNotExist() {
+        Box box = new Box(-1, 10);
+        boolean isExist = box.isExist();
+        assertThat(isExist).isFalse()
+                .isNotNull();
+    }
+    @Test
+    void whenAreaOfTetrahedronIsZero() {
+        Box box = new Box(0, 1);
+        double area = box.getArea();
+        assertThat(area).isGreaterThan(6)
+                .isEqualTo(4*Math.PI)
+                .isPositive()
+                .isNotNull();
+    }
+    @Test
+    void whenAreaOfTetrahedronIsFour() {
+        Box box = new Box(4, 1);
+        double area = box.getArea();
+        assertThat(area).isGreaterThan(1)
+                .isEqualTo(Math.sqrt(3))
+                .isPositive()
+                .isNotNull();
     }
 
 }
